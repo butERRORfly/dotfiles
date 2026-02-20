05b27e229cee55af4f51fe454056050cce6b3781
butERRORfly <progamer_228@inbox.ru>
2026-02-20 18:58:41 +0300

    Fix setup script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Setup neovim.
+# setup neovim.
 mkdir -p ~/.config/nvim
 ln -sf "$PWD/.config/nvim" ~/.config/nvim
 
-# Setup tmux.
+# setup tmux.
 mkdir -p ~/.config/tmux
 ln -sf "$PWD/.config/tmux" ~/.config/tmux
 
-# Success.
-echo "Dotfiles successfully installed."
+# success.
+echo "dotfiles successfully installed."
