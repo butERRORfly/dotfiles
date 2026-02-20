9907a98de2a0b98d5f0b241167b0100de0605450
butERRORfly <progamer_228@inbox.ru>
2026-02-20 19:01:55 +0300

    Fix setup script

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # setup neovim.
 mkdir -p ~/.config/nvim
-ln -sf "$PWD/.config/nvim" ~/.config/nvim
+ln -sf "$DOTFILES_DIR/.config/nvim" ~/.config/nvim
 
 # setup tmux.
 mkdir -p ~/.config/tmux
-ln -sf "$PWD/.config/tmux" ~/.config/tmux
+ln -sf "$DOTFILES_DIR/.config/tmux" ~/.config/tmux
 
 # success.
-echo "dotfiles successfully installed."
+echo "Dotfiles successfully installed."
